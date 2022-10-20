440972a9be1df203176a42b934a0b60b71c3ab15
Kseniia Prytkova <prytkovakseniia@gmail.com>
2022-10-20 22:07:12 +0100

    add routes and Info page content 🌱

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+export const Navbar = () => (
+	<nav className="navbar navbar-dark bg-primary navbar-expand-lg">
+		<div className="navbar-brand">GitHub Search</div>
+		<ul className="navbar-nav">
+			<li className="nav-item">
+				<Link to="/" className="nav-link">
+					Home
+				</Link>
+			</li>
+			<li className="nav-item">
+				<Link to="/info" className="nav-link">
+					Info
+				</Link>
+			</li>
+		</ul>
+	</nav>
+);
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+export const About = () => {
+	return (
+		<div className="p-5 mb-4 bg-light rounded-3">
+			<div Name="container-fluid py-5">
+				<h1 Name="display-5 fw-bold">Information</h1>
+				<p Name="col-md-8 fs-4">
+					Application version: <strong>1.0.0</strong>
+				</p>
+			</div>
+		</div>
+	);
+};
