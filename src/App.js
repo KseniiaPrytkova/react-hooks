0dfc1eb28e3952d8376bbe674782e41fbfa4a325
Kseniia Prytkova <prytkovakseniia@gmail.com>
2022-10-21 00:50:39 +0100

    add alert

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
+import { Alert } from "./components/Alert";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { About } from "./pages/About";
 import { Home } from "./pages/Home";
 import { Profile } from "./pages/Profile";
+import { AlertState } from "./context/alert/AlertState";
 
 function App() {
 	return (
-		<BrowserRouter>
-			<Navbar />
-			<div className="container pt-4">
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/about" element={<About />} />
-					<Route path="/profile/:name" element={<Profile />} />
-				</Routes>
-			</div>
-		</BrowserRouter>
+		<AlertState>
+			<BrowserRouter>
+				<Navbar />
+
+				<div className="container pt-4">
+					<Alert alert={{ text: "Test alert" }} />
+
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/about" element={<About />} />
+						<Route path="/profile/:name" element={<Profile />} />
+					</Routes>
+				</div>
+			</BrowserRouter>
+		</AlertState>
 	);
 }
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,20 @@
+import { useContext } from "react";
+import { AlertContext } from "../context/alert/alertContext";
+
 export const Search = () => {
+	const { show } = useContext(AlertContext);
+
+	const onSubmit = (event) => {
+		show("qqq");
+	};
+
 	return (
-		<div className="input-group mb-4">
+		<div className="mb-4">
 			<input
 				type="text"
 				className="form-control"
 				placeholder="Enter username..."
+				onKeyUp={onSubmit}
 			/>
 		</div>
 	);
