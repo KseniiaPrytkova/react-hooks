918547249df4ae7184cfdd295831f10ec02c9b79
Kseniia Prytkova <prytkovakseniia@gmail.com>
2022-10-20 23:25:26 +0100

    add card

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,21 @@
 import { Link } from "react-router-dom";
 
 export const Navbar = () => (
-	<nav className="navbar navbar-dark bg-primary navbar-expand-lg">
-		<div className="navbar-brand">GitHub Search</div>
-		<ul className="navbar-nav">
-			<li className="nav-item">
-				<Link to="/" className="nav-link">
-					Home
-				</Link>
-			</li>
-			<li className="nav-item">
-				<Link to="/info" className="nav-link">
-					Info
-				</Link>
-			</li>
-		</ul>
+	<nav className="navbar navbar-dark bg-primary navbar-expand-sm">
+		<div className="container-fluid">
+			<div className="navbar-brand">GitHub Search</div>
+			<ul className="navbar-nav">
+				<li className="nav-item">
+					<Link to="/" className="nav-link">
+						Home
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link to="/info" className="nav-link">
+						Info
+					</Link>
+				</li>
+			</ul>
+		</div>
 	</nav>
 );
---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+export const Search = () => {
+	return (
+		<div className="input-group mb-4">
+			<input
+				type="text"
+				className="form-control"
+				placeholder="Enter username..."
+			/>
+		</div>
+	);
+};
